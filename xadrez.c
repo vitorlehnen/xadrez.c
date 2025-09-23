bb67e4d8e38b063b1c124e2e0f3d3ceb45924ea3
Vitor Lehnen <150603968+vitorlehnen@users.noreply.github.com>
2025-09-23 23:38:09 +0000

    feat: adicionado movimento da torre, bispo e rainha

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
 
+int main(){
+
+    int controladorTorre = 0, controladorBispo = 0;
+
+    //Movimento da torre
+    while (controladorTorre < 5)
+    {
+        printf("Direita\n");
+        controladorTorre++;
+    }
+    
+    //Movimento do bispo
+    do
+    {
+        printf("Cima, Direita\n");
+        controladorBispo++;
+    } while (controladorBispo < 5);
+
+    //Movimento da rainha
+    for (int count = 0; count < 8; count++)
+    {
+        printf("Esquerda\n");
+    }
+
+    return 0;
+}
